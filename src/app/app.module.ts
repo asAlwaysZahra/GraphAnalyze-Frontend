7777a6e45a9f272ab1dc1e6e14604040b6413776
Mojtaba Erfan Rad <mojmal_mojar@yahoo.com>
2024-08-12 12:51:44 +0330

    fix(login): added vis nodes for login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { MatIconModule } from '@angular/material/icon';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatButtonModule } from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { UserModule } from './user/user.module';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    MatButtonModule,
-    MatDividerModule,
-    MatIconModule,
-    UserModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule, UserModule],
   providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
