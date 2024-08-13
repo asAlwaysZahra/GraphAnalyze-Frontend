04e79ef757ac274a17817b7fb0a5661f696d7b4e
Mojtaba Erfan Rad <mojmal_mojar@yahoo.com>
2024-08-13 09:36:42 +0330

    feat(Login): structure of login page added (#3)
    * feat(user): add user module
    
    * fix(user): service test add - bug
    
    * fix(UI): added material design system
    
    * fix(UI): added material to package manager
    
    * fix(Packages): added vis
    
    * fix(user): added login component
    
    * fix(Login): show login page on /login route
    
    * fix(input): added general style for login form inputs
    
    * fix(login): added vis nodes for login page
    
    * feat(login): made login page
    
    * fix(login): fixed graph physics
    
    * fix(config): fixed angular.json for building project with vis beeing CommonJS
    
    * fix(lint): add node and edge interfaces
    
    * fix(lint): added datatype to nodes and edges
    
    * fix(styles): convert to nested
    
    * fix(login): added types of variables
    
    ---------
    
    Co-authored-by: Zahra Masoumi <zahraamasoumi@gmail.com>

---
+++ src/app/user/user.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './components/login/login.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
+@NgModule({
+  declarations: [LoginComponent],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
+  exports: [MatFormFieldModule, MatInputModule, MatIconModule],
+})
+export class UserModule {}
