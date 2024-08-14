d431a402c05beab961cdcb703b57dbad10232ebe
Mojtaba Erfan Rad <mojmal_mojar@yahoo.com>
2024-08-14 12:13:11 +0330

    feat(dashboard): first dashboard structure created
    * feat(user): add user module
    
    * fix(user): service test add - bug
    
    * fix(UI): added material design system
    
    * fix(UI): added material to package manager
    
    * fix(Packages): added vis
    
    * fix(user): added login component
    
    * fix(Login): show login page on /login route
    
    * fix(input): added general style for login form inputs
    
    * fix(login): added vis nodes for login page
    
    * feat(login): made login page
    
    * fix(login): fixed graph physics
    
    * fix(config): fixed angular.json for building project with vis beeing CommonJS
    
    * fix(lint): add node and edge interfaces
    
    * fix(lint): added datatype to nodes and edges
    
    * fix(styles): convert to nested
    
    * fix(login): added types of variables
    
    * fix(login): fix button style
    
    * fix(tests): fixed login component test
    
    * fix(css): added reset style
    
    * fix(login): fix name of field
    
    * fix(login): fix login page style
    
    * fix(login): add method for hiding pass
    
    * fix(login): fix bug
    
    * fix(login): fixed showing login form
    
    * fix(login): remember me button name fixed
    
    * fix(login): fixed vis nodes group background color
    
    * feat(ui): add dark and light themes
    
    * fix(logo): added logo
    
    * fix(login): fixed tests and some styles
    
    * fix(lint): fixed lint
    
    * feat(dashboard): added main page of the dashboard
    
    * fix(dashboard): added dashboard side nav
    
    * feat(dashboard): fixed routing for dashboard
    
    ---------
    
    Co-authored-by: Zahra Masoumi <zahraamasoumi@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './user/components/login/login.component';
+import { DashboardComponent } from './user/components/dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent, title: 'StarData | Login' },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    title: 'StarData | Dashboard',
+    children: [
+      {
+        path: '',
+        component: LoginComponent,
+        title: 'StarData | Dashboard',
+      },
+      {
+        path: 'add-user',
+        component: LoginComponent,
+        title: 'StarData | Add User',
+      },
+    ],
+  },
 ];
 
 @NgModule({
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -6,10 +6,16 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatRippleModule } from '@angular/material/core';
+import { AppRoutingModule } from '../app-routing.module';
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, DashboardComponent],
   imports: [
     CommonModule,
     MatFormFieldModule,
@@ -18,7 +24,12 @@ import { FormsModule } from '@angular/forms';
     MatButtonModule,
     MatCheckboxModule,
     FormsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    ReactiveFormsModule,
+    MatListModule,
+    MatRippleModule,
+    AppRoutingModule,
   ],
-  exports: [MatFormFieldModule, MatInputModule, MatIconModule],
 })
 export class UserModule {}
