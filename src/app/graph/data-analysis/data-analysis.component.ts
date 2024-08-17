0a587af95416ff8d207aaa62dce209bbdfee9a19
Mojtaba Erfan Rad <mojmal_mojar@yahoo.com>
2024-08-17 12:13:10 +0330

    fix(data-analysis): added search form

--- src/app/graph/data-analysis/data-analysis.component.ts
+++ src/app/graph/data-analysis/data-analysis.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import { Network, DataSet, Node, Edge, Options, Data } from 'vis';
 
 @Component({
   selector: 'app-data-analysis',
   templateUrl: './data-analysis.component.html',
-  styleUrl: './data-analysis.component.scss'
+  styleUrl: './data-analysis.component.scss',
 })
-export class DataAnalysisComponent {
+export class DataAnalysisComponent implements AfterViewInit {
+  @ViewChild('network') el!: ElementRef;
+  private networkInstance!: Network;
+  search = '';
 
+  ngAfterViewInit() {
+    const container = this.el.nativeElement;
+    // create some nodes
+    const nodes = new DataSet<Node>([
+      { id: 0, label: 'Mamad' },
+      { id: 1, label: 'Ali' },
+      { id: 2, label: 'Hasan' },
+    ] as unknown as Node[]);
+
+    // create some edges
+    const edges = new DataSet<Edge>([
+      { from: 1, to: 0, label: '100 toman' },
+      { from: 1, to: 2, label: '100 toman' },
+      { from: 2, to: 0, label: '100 toman' },
+    ] as Edge[]);
+
+    const data: Data = { nodes, edges };
+
+    // create a network
+    const options: Options = {
+      physics: false,
+      edges: { arrows: 'to' },
+      nodes: {
+        shape: 'dot',
+      },
+    };
+
+    this.networkInstance = new Network(container, data, options);
+  }
 }
--- src/app/graph/graph.module.ts
+++ src/app/graph/graph.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DataAnalysisComponent } from './data-analysis/data-analysis.component';
+import { SharedModule } from '../shared/shared.module';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [DataAnalysisComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    SharedModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+  ],
 })
 export class GraphModule {}
