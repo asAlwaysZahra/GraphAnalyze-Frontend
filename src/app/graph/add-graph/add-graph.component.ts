6807bccda9bc494b8c9377c04e1ec30a97ef3284
Mojtaba Erfan Rad <mojmal_mojar@yahoo.com>
2024-08-24 11:09:57 +0330

    fix(upload): drag drop fixed

--- src/app/graph/add-graph/add-graph.component.ts
+++ src/app/graph/add-graph/add-graph.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-add-graph',
   templateUrl: './add-graph.component.html',
-  styleUrl: './add-graph.component.scss'
+  styleUrl: './add-graph.component.scss',
 })
 export class AddGraphComponent {
+  @ViewChild('file') el!: ElementRef;
+  isHighlighted = false;
 
+  highlight(event: Event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    this.isHighlighted = true;
+  }
+
+  unhighlight(event: Event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    this.isHighlighted = false;
+  }
+
+  dropped(event: Event) {
+    console.log(event);
+  }
 }
