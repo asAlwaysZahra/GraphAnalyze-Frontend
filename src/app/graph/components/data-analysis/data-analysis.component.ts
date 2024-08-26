526e5870f6b7367fde3257318a9f5515a47b1cae
Mojtaba Erfan Rad <mojmal_mojar@yahoo.com>
2024-08-26 15:09:20 +0330

    feat(show-graph): added pagination to show graphs

--- src/app/graph/components/data-analysis/data-analysis.component.ts
+++ src/app/graph/components/data-analysis/data-analysis.component.ts
@@ -47,6 +47,34 @@ export class DataAnalysisComponent implements AfterViewInit {
     this.loadGraphService.getAllNodes(e.pageIndex);
   }
 
+  nodes = new DataSet<Node>([
+    { id: 0, label: '16546220216446' },
+    { id: 1, label: '16546220216446' },
+    { id: 2, label: '16546220216446' },
+    { id: 50, label: '16546220216446' },
+  ] as unknown as Node[]);
+  edges = new DataSet<Edge>([
+    { id: 1, from: 1, to: 0, label: '100,000 تومان' },
+    { id: 2, from: 1, to: 2, label: '150,000 تومان' },
+    { id: 3, from: 2, to: 0, label: '250,000 تومان' },
+    { id: 4, from: 50, to: 0, label: '3,000,000 تومان' },
+  ] as Edge[]);
+  data: Data = { nodes: this.nodes, edges: this.edges };
+
+  length!: number;
+  pageIndex = 0;
+
+  constructor(
+    private themeService: ThemeService,
+    private loadGraphService: LoadGraphService
+  ) {}
+
+  handlePageEvent(e: PageEvent) {
+    this.pageIndex = e.pageIndex;
+    this.length = e.length;
+    this.loadGraphService.getAllNodes(e.pageIndex);
+  }
+
   ngAfterViewInit() {
     this.createGraph();
 
