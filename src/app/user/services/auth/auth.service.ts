a48abda132601af664424083bce155ac7a6ae4e9
Zahra Masoumi <zahraamasoumi@gmail.com>
2024-08-26 13:00:19 +0330

    fix(service)

--- src/app/user/services/auth/auth.service.ts
+++ src/app/user/services/auth/auth.service.ts
@@ -1,11 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable, Subject, tap } from 'rxjs';
-import {
-  LoginRequest,
-  LoginResponse,
-  UserPermissions,
-} from '../../models/User';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { LoginRequest, UserPermissions } from '../../models/User';
 import { environment } from '../../../../../api-config/api-url';
 
 @Injectable({
@@ -13,32 +9,15 @@ import { environment } from '../../../../../api-config/api-url';
 })
 export class AuthService {
   private apiUrl = environment.apiUrl + '/api/User';
-
-  private userData = new Subject<LoginResponse>();
-  private isLoggedIn = new BehaviorSubject<boolean>(false);
   private permissions = new BehaviorSubject<UserPermissions | null>(null);
-
-  isLoggedIn$ = this.isLoggedIn.asObservable();
-  userData$ = this.userData.asObservable();
   permissions$ = this.permissions.asObservable();
 
   constructor(private http: HttpClient) {}
 
-  // get isLoggedIn$() {
-  //   return this.isLoggedIn.asObservable();
-  // }
-
-  login(loginRequest: LoginRequest): Observable<LoginResponse> {
-    return this.http
-      .post<LoginResponse>(this.apiUrl + '/login', loginRequest, {
-        withCredentials: true,
-      })
-      .pipe(
-        tap((response) => {
-          this.userData.next(response);
-          this.isLoggedIn.next(true);
-        })
-      );
+  login(loginRequest: LoginRequest): Observable<void> {
+    return this.http.post<void>(this.apiUrl + '/login', loginRequest, {
+      withCredentials: true,
+    });
   }
 
   getPermissions() {
@@ -49,12 +28,10 @@ export class AuthService {
         })
         .pipe(
           tap((response) => {
-            if (response.firstName == null) this.isLoggedIn.next(false);
-            else {
-              this.isLoggedIn.next(true);
+            if (response.firstName) {
               this.permissions.next(response);
             }
-          })
+          }),
         );
     }
     return this.permissions$;
@@ -65,9 +42,8 @@ export class AuthService {
       .post(this.apiUrl + '/logout', null, { withCredentials: true })
       .pipe(
         tap(() => {
-          this.isLoggedIn.next(false);
           this.permissions.next(null);
-        })
+        }),
       );
   }
 }
