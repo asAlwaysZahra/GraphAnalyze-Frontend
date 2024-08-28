fc45db7f680ab3529d920004d07cc0dcec65ad4e
Zahra Masoumi <zahraamasoumi@gmail.com>
2024-08-28 23:08:57 +0330

    fix(test) fix should create tests of shared components and services

--- src/app/shared/components/dashboard-header/dashboard-header.component.spec.ts
+++ src/app/shared/components/dashboard-header/dashboard-header.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardHeaderComponent } from './dashboard-header.component';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 describe('DashboardHeaderComponent', () => {
   let component: DashboardHeaderComponent;
@@ -8,9 +10,9 @@ describe('DashboardHeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [DashboardHeaderComponent]
-    })
-    .compileComponents();
+      declarations: [DashboardHeaderComponent],
+      imports: [MatIconModule, RouterModule.forRoot([])],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(DashboardHeaderComponent);
     component = fixture.componentInstance;
--- src/app/shared/services/theme.service.spec.ts
+++ src/app/shared/services/theme.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ThemeService } from './theme.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('ThemeService', () => {
   let service: ThemeService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+    });
     service = TestBed.inject(ThemeService);
   });
 
