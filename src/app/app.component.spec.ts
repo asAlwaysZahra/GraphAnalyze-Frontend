cced04e456db21e7b3b601c9a3237ee6e6688307
Mojtaba Erfan Rad <mojmal_mojar@yahoo.com>
2024-08-28 09:58:26 +0330

    fix(test)

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterModule.forRoot([])],
+      imports: [RouterModule.forRoot([]), MatProgressBarModule],
       declarations: [AppComponent],
     }).compileComponents();
   });
