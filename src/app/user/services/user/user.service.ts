dc2cb73f6cd18bbcf684f46434ed7889858e5465
Zahra Masoumi <zahraamasoumi@gmail.com>
2024-09-06 19:10:53 +0330

    feat(profile): add profile header and image

--- src/app/user/services/user/user.service.ts
+++ src/app/user/services/user/user.service.ts
@@ -1,10 +1,10 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { ForgetPasswordRequest, NewPasswordRequest } from '../../models/User';
-import { environment } from '../../../../../api-config/api-url';
-import { LoadingService } from '../../../shared/services/loading.service';
-import { UserInformation } from '../../models/ManageUsers';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {ForgetPasswordRequest, NewPasswordRequest} from '../../models/User';
+import {environment} from '../../../../../api-config/api-url';
+import {LoadingService} from '../../../shared/services/loading.service';
+import {UserInformation} from '../../models/ManageUsers';
 
 @Injectable({
   providedIn: 'root',
@@ -15,7 +15,8 @@ export class UserService {
   constructor(
     private http: HttpClient,
     private loadingService: LoadingService,
-  ) {}
+  ) {
+  }
 
   forgetPassword(request: ForgetPasswordRequest): Observable<void> {
     this.loadingService.setLoading(true);
@@ -44,6 +45,17 @@ export class UserService {
   updateUser(userInfo: UserInformation) {
     this.loadingService.setLoading(true);
     return this.http.put(`${this.apiUrl}/update-user`, userInfo, {
+      withCredentials: true,
+    });
+  }
+
+  uploadImage(file: File) {
+    this.loadingService.setLoading(true);
+    const formData: FormData = new FormData();
+
+    formData.append('file', file);
+
+    return this.http.post(this.apiUrl + '/upload-image', formData, {
       withCredentials: true,
     });
   }
