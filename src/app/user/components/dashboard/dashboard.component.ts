56354189e872e07baf3570749b4c2e99bb764f67
Mojtaba Erfan Rad <mojmal_mojar@yahoo.com>
2024-09-08 21:27:19 +0330

    fix(menu): show menu links based on permission

--- src/app/user/components/dashboard/dashboard.component.ts
+++ src/app/user/components/dashboard/dashboard.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth/auth.service';
 import { Router } from '@angular/router';
 import { LoadingService } from '../../../shared/services/loading.service';
+import { LINKS } from './menu';
 
 /** @title Responsive sidenav */
 @Component({
@@ -9,12 +10,21 @@ import { LoadingService } from '../../../shared/services/loading.service';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  protected readonly links = LINKS;
+  protected permissions: string[] = [];
+
   constructor(
     private authService: AuthService,
     private router: Router,
     private loadingService: LoadingService
   ) {}
+
+  ngOnInit(): void {
+    this.authService.getPermissions().subscribe((data) => {
+      this.permissions = data!.permission;
+    });
+  }
 
   logoutClick() {
     this.loadingService.setLoading(true);
